177722f45b6439ae1b50571c165f78a555823feb
hoseungzzang <wnghtmd99@naver.com>
2023-02-16 22:24:24 +0900

    feat(store) : 판매자 상태현황 리스트 구현중

--- src/apis/seller/StoreApis.js
+++ src/apis/seller/StoreApis.js
@@ -7,3 +7,18 @@ export const insertRequestHistorySeller = async (requestHistoryData) => {
   const res = await axios.post(`${domain}/api/seller/store/request`, requestHistoryData)
   return res.data
 }
+
+export const getStoreStatus = async ( id ) => {
+  const res = await axios.get(`${ domain }/api/seller/store/status/${id}`)
+  return res.data
+}
+
+export const getStoreStatusList = async ( listInfo ) => {
+  const res = await axios.get(`${ domain }/api/seller/store/status`, {
+    params: {
+      page: listInfo.page || 1, size: listInfo.size || 10, id: listInfo.id
+    }
+  })
+  return res.data
+}
+
--- src/components/seller/store/StatusComponent.vue
+++ src/components/seller/store/StatusComponent.vue
@@ -1,13 +1,74 @@
 <template>
   <v-col cols="12">
-    <v-card min-height="85vh">
-      <h1>영업 상태 현황 페이지</h1>
+    <v-card color="grey-darken-2" min-height="85vh">
+      <div>
+        <v-card class="ma-10 pa-10 font-weight-bold text-h4">우리 매장은 {{ storeStatus.seName }}은 <h2 style="display: inline; color: red">
+          {{ storeStatus.seStatus }}</h2> 상태입니다!</v-card>
+        <v-card class="ma-10">
+          <v-table class="ma-4">
+            <thead>
+            <tr class="bg-amber-accent-2">
+              <th class="text-center font-weight-bold">요청번호</th>
+              <th class="text-center font-weight-bold">종류</th>
+              <th class="text-center font-weight-bold">사유</th>
+              <th class="text-center font-weight-bold">상태</th>
+              <th class="text-center font-weight-bold">등록일</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="history, index in historyList" :key="history.pmNo">
+              <td class="text-center">{{ history.rhContent }}</td>
+              <td class="text-center">{{ history.rhReason }}</td>
+              <td class="text-center">{{ history.rhStatus }}</td>
+              <td class="text-center">{{ history.rhCreateDt }}</td>
+            </tr>
+            </tbody>
+          </v-table>
+          <v-pagination v-model="pageInfo.page" :show-first-last-page="true"
+                        :total-visible="pageInfo.size" :length="pageInfo.last" :start="pageInfo.start"
+                        @click="handleClickPage(pageInfo)">
+          </v-pagination>
+        </v-card>
+      </div>
     </v-card>
   </v-col>
 </template>
 
 <script setup>
+import {onMounted, ref} from "vue";
+import {getPlaceRoomStatus, getPlaceRoomStatusList} from "@/apis/place/PlaceApis";
+import {getStoreStatus} from "@/apis/seller/StoreApis";
 
+
+const props = defineProps(['listInfo'])
+const emits = defineEmits(['handleRouterList'])
+const pageInfo = ref([{ page: null, size: null, last: null, start: 1 }])
+const storeStatus = ref({})
+const historyList = ref([])
+
+
+const handleClickPage = ( pageInfo ) => {
+  emits('handleRouterList', pageInfo)
+}
+
+const getSellerStatus = async () => {
+  const data = await getStoreStatus(props.listInfo.id )
+  storeStatus.value = data.data
+}
+
+const getSellerStatusHistory = async () => {
+  const data = await getPlaceRoomStatusList( props.listInfo )
+  historyList.value = data.dtoList
+  pageInfo.value.page = data.page
+  pageInfo.value.size = data.size
+  pageInfo.value.last = data.last
+}
+
+
+onMounted(() => {
+  getSellerStatus()
+  getSellerStatusHistory()
+})
 </script>
 
 <style scoped>
